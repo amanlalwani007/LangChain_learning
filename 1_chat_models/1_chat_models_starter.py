6043cfa074bf15e10802ba019913c976dcc3935f
amanlalwani007 <amanlalwani0807@gmail.com>
2025-02-11 11:49:34 +0530

    reverted

--- 1_chat_models/1_chat_models_starter.py
+++
@@ -1,9 +0,0 @@
-from langchain_openai import ChatOpenAI
-from dotenv import load_dotenv
-
-load_dotenv()
-llm = ChatOpenAI(model="gpt-4o")
-
-
-result = llm.invoke("What is square root of 49")
-print(result.content)
